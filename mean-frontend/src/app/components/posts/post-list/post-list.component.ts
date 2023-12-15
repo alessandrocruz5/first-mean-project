4429310441609c8e1d97812cff4cabac7b998705
alessandrocruz5 <alessandrorafaelcruz@gmail.com>
2023-12-15 16:12:02 +0800

    Pagination

--- mean-frontend/src/app/components/posts/post-list/post-list.component.ts
+++ mean-frontend/src/app/components/posts/post-list/post-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { PageEvent } from '@angular/material/paginator';
 import { Subscription } from 'rxjs';
 import { Post } from 'src/app/models/post/post.model';
 import { PostsService } from 'src/app/services/posts.service';
@@ -11,18 +12,23 @@ import { PostsService } from 'src/app/services/posts.service';
 export class PostListComponent implements OnInit, OnDestroy {
   posts: Post[] = [];
   isLoading = false;
+  totalPosts = 0;
+  postsPerPage = 2;
+  currPage = 1;
+  pageSizeOptions = [1, 2, 5, 10];
   private postsSub!: Subscription;
 
   constructor(public postsService: PostsService) {}
 
   ngOnInit() {
     this.isLoading = true;
-    this.postsService.getPosts();
+    this.postsService.getPosts(this.postsPerPage, 1);
     this.postsSub = this.postsService
       .getPostsUpdatedListener()
-      .subscribe((posts: Post[]) => {
+      .subscribe((postData: { posts: Post[]; postCount: number }) => {
         this.isLoading = false;
-        this.posts = posts;
+        this.totalPosts = postData.postCount;
+        this.posts = postData.posts;
       });
   }
 
@@ -31,6 +37,15 @@ export class PostListComponent implements OnInit, OnDestroy {
   }
 
   onDelete(postId: string) {
-    this.postsService.deletePost(postId);
+    this.postsService.deletePost(postId).subscribe(() => {
+      this.postsService.getPosts(this.postsPerPage, this.currPage);
+    });
+  }
+
+  onChangePage(pageData: PageEvent) {
+    this.isLoading = true;
+    this.currPage = pageData.pageIndex + 1;
+    this.postsPerPage = pageData.pageSize;
+    this.postsService.getPosts(this.postsPerPage, this.currPage);
   }
 }
